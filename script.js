70b762df01f30813ae0e74b297d3fa1e8a3977d4
kwabsvlado <vlado.cokina1@gmail.com>
2023-03-15 20:48:03 +0100

    Countdown

---
+++ script.js
@@ -0,0 +1,76 @@
+const days = document.querySelector('.days')
+const hours = document.querySelector(".hours");
+const minutes = document.querySelector('.minutes')
+const seconds = document.querySelector('.seconds')
+
+let timeLeft = {
+ d: 0, 
+ h: 0,
+ m: 0, 
+ s: 0
+}
+
+let totalSeconds
+
+function init(){
+  totalSeconds = Math.floor((new Date("July 01, 2023 00:00:00") - new Date()) / 1000)
+  setTimeLeft()
+  let interval = setInterval(() => {
+   if(totalSeconds < 0){
+    clearInterval(interval)
+   }
+   countTime()
+  }, 1000)
+}
+
+function countTime (){
+ if(totalSeconds > 0){
+   --timeLeft.s
+   if(timeLeft.m >= 0 && timeLeft.s < 0){
+   timeLeft.s = 59
+   --timeLeft.m
+   if(timeLeft.h >= 0 && timeLeft.m < 0){
+    timeLeft.m = 59
+    --timeLeft.h
+    if(timeLeft.d >= 0 && timeLeft.h < 0){
+   timeLeft.h = 23
+   --timeLeft.d
+    }
+   }
+  }
+ }
+ --totalSeconds
+ printTime();
+}
+
+function printTime (){
+ days.innerText = timeLeft.d
+ hours.innerText = timeLeft.h
+ minutes.innerText = timeLeft.m
+ seconds.innerText = timeLeft.s
+
+ if (timeLeft.h < 10) {
+   hours.innerText = "0" + timeLeft.h;
+ }if (timeLeft.m < 10) {
+   minutes.innerText = "0" + timeLeft.m;
+ }if (timeLeft.s < 10) {
+   seconds.innerText = "0" + timeLeft.s;
+ }
+ 
+
+ 
+}
+
+
+
+
+function setTimeLeft (){
+ timeLeft.d = Math.floor(totalSeconds / (60 * 60 * 24))
+ timeLeft.h = Math.floor(totalSeconds / (60 * 60) % 24)
+ timeLeft.m = Math.floor(totalSeconds / (60) % 60 )
+ timeLeft.s = Math.floor(totalSeconds % 60)
+ 
+}
+
+init()
+
